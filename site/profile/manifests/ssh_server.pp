5a42e83d9a37893ba55e36b5327dbc1813120451
JayemNo <67764584+JayemNo@users.noreply.github.com>
2021-06-11 23:00:29 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCpaLUhpdFRD28WO0EicXysUxWqj5BqWZRgsQABl9iW9fb1y1nKdZhfYMyOKkwyhQPqYtAcnB1yDo2pY5RCh2ETUpwTMP9O7pq6fiJtwRMsz1mbz+xKjhtZBTyXd0ZObY8KSZ+KAmlzsiMuSqVHFzO+yj2dpFelFuN1hEoCBnvl0vgvZ+x9hpioHRhKIdIVbmGYZWNxkGTjEW9Y5wbHXJ8nPbzz0mol3Mwc7rG0zXug/VP+4KRsKko9q392QDzHfL+hBX5uz/sKhODWGVY/pjmFdHjJNcAszHHCtV+keGZuaSl7ZGTxSCZ6i3oKCEJBbvywcGTGmzIUjbLoyso3+8hd root@master.puppet.vm',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCpaLUhpdFRD28WO0EicXysUxWqj5BqWZRgsQABl9iW9fb1y1nKdZhfYMyOKkwyhQPqYtAcnB1yDo2pY5RCh2ETUpwTMP9O7pq6fiJtwRMsz1mbz+xKjhtZBTyXd0ZObY8KSZ+KAmlzsiMuSqVHFzO+yj2dpFelFuN1hEoCBnvl0vgvZ+x9hpioHRhKIdIVbmGYZWNxkGTjEW9Y5wbHXJ8nPbzz0mol3Mwc7rG0zXug/VP+4KRsKko9q392QDzHfL+hBX5uz/sKhODWGVY/pjmFdHjJNcAszHHCtV+keGZuaSl7ZGTxSCZ6i3oKCEJBbvywcGTGmzIUjbLoyso3+8hd',
 	}  
 }
